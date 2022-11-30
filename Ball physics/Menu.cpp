a3287fdc9a60ad70459308237f04bf57c801842b
olager <oleg.sheverdov2004@mail.ru>
2022-12-01 02:05:46 +0300

    1.0.6

--- Ball physics/Menu.cpp
+++ Ball physics/Menu.cpp
@@ -20,7 +20,6 @@ void Menu::callMenu(sf::RenderWindow &window)//вызов метода меню
 	
 	sf::Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), menu4(menuTexture4), menuBG(menuBackground);
 	bool isMenu = 1;
-	int menuNum = 0;
 	menu1.setPosition(100, 200); //установка позиций
 	menu2.setPosition(100, 300);
 	menu3.setPosition(100, 400);
@@ -36,10 +35,6 @@ void Menu::callMenu(sf::RenderWindow &window)//вызов метода меню
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
 		menu1.setColor(sf::Color::White);//сохранение оригинального цвета текстуры
 		menu2.setColor(sf::Color::White);
 		menu3.setColor(sf::Color::White);
@@ -53,10 +48,22 @@ void Menu::callMenu(sf::RenderWindow &window)//вызов метода меню
 		if (sf::IntRect(108, 500, 91, 47).contains(sf::Mouse::getPosition(window))) { menu4.setColor(sf::Color::Blue); menuNum = 4; }
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			if (menuNum == 1) isMenu = false;//1 уровень
-			if (menuNum == 2) isMenu = false;//2 уровень
-			if (menuNum == 3) isMenu = false;//3 уровень
-			if (menuNum == 4) { window.close(); isMenu = false; }//4 выход 
+			if (menuNum == 1)//1 уровень
+			{
+				MenuInProgress(window, menuNum);
+			}
+			if (menuNum == 2)//2 уровень
+			{
+				MenuInProgress(window, menuNum);
+			}
+			if (menuNum == 3)//3 уровень
+			{
+				MenuInProgress(window, menuNum);
+			}
+			if (menuNum == 4) 
+			{ 
+				window.close(); isMenu = false;
+			}//4 выход 
 		}
 
 		window.draw(menuBG);//отрисовка
@@ -71,9 +78,40 @@ void Menu::callMenu(sf::RenderWindow &window)//вызов метода меню
 
 
 
-void Menu::MenuInProgress(sf::RenderWindow& window)
+void Menu::MenuInProgress(sf::RenderWindow& window,int menuNum)
 {
-
+	
+	if (menuNum == 1)//если земля
+	{
+		background.loadFromFile("F:/курсач/BallPhysX/images/bg1.jpg");
+	}
+	else if (menuNum == 2)//марс
+	{
+		background.loadFromFile("F:/курсач/BallPhysX/images/bg2.jpg");
+	}
+	else//луна
+	{
+		background.loadFromFile("F:/курсач/BallPhysX/images/bg3.jpg");
+	}
+	sf::Sprite background(background);
+	background.setPosition(0,0);//установка бекграунда
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))//если нажать esc выйдет в меню
+		{
+			return;
+		}
+		
+		window.clear(sf::Color(129, 181, 221));//очистка экрана
+		window.draw(background);//отрисовка
+		window.display();//
+	}
 
 }
 
--- Ball physics/Menu.h
+++ Ball physics/Menu.h
@@ -2,11 +2,12 @@
 #include <SFML/Graphics.hpp>
 class Menu
 {
-	sf::Texture menuTexture1, menuTexture2, menuTexture3, menuTexture4, menuBackground;
+	sf::Texture menuTexture1, menuTexture2, menuTexture3, menuTexture4, menuBackground,background;
+	int menuNum;
 public:
 	Menu();
 	~Menu();
 	void callMenu(sf::RenderWindow &window);
-	void MenuInProgress(sf::RenderWindow &window);
+	void MenuInProgress(sf::RenderWindow &window,int menuNum);
 };
 
